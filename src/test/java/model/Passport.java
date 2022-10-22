252b483926dc6b4ffc89751366499b9899df2953
FkkfRf <Fkkf.Rf@gmail.com>
2022-10-22 21:49:27 +0300

    File Project

---
+++ src/test/java/model/Passport.java
@@ -0,0 +1,13 @@
+package rf.fkkf.model;
+
+public class Passport {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String gender ;
+    public int okato;
+    public int issueYear;
+    public String number;
+
+
+}
